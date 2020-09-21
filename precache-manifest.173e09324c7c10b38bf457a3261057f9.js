ed221736077e12f9774dd2f542b2200a62e03e20
JonathanGzzBen <jonathangzzben@gmail.com>
2020-09-20 19:54:20 -0500

    Updates

---
+++ precache-manifest.173e09324c7c10b38bf457a3261057f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27b52946008ac206e1a26db4d9e6b66",
+    "url": "/youtube-downloader-react/index.html"
+  },
+  {
+    "revision": "c879be023b31ccfa7e0d",
+    "url": "/youtube-downloader-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "68b500af7dd187911323",
+    "url": "/youtube-downloader-react/static/js/2.b7823d10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-downloader-react/static/js/2.b7823d10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c879be023b31ccfa7e0d",
+    "url": "/youtube-downloader-react/static/js/main.c2095a12.chunk.js"
+  },
+  {
+    "revision": "f26398960838d1e47e46",
+    "url": "/youtube-downloader-react/static/js/runtime-main.0d0edddb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/youtube-downloader-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
