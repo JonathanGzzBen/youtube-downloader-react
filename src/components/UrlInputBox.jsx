e7c108954bc4758a821b663c1d962232398db506
JonathanGzzBen <jonathangzzben@gmail.com>
2020-09-27 15:47:33 -0500

    Modify UrlInputBox to have Search button instead of Download

--- src/components/UrlInputBox.jsx
+++ src/components/UrlInputBox.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import urlInputBoxStyles from "../styles/UrlInputBox.module.css";
 
 const UrlInputBox = (props) => {
-  const { onDownloadButtonClick } = props;
+  const { onSearchButtonClick } = props;
   return (
     <div className={urlInputBoxStyles["url-input-box"]}>
       <input id="url-box" type="text" placeholder="Enter video's url here..." />
       <button
         onClick={() =>
-          onDownloadButtonClick(document.getElementById("url-box").value)
+          onSearchButtonClick(document.getElementById("url-box").value)
         }
       >
-        Download
+        Search
       </button>
     </div>
   );
--- src/components/routes/Full.jsx
+++ src/components/routes/Full.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import api from "../../services/api";
 import UrlInputBox from "../UrlInputBox";
+import VideoDetails from "../VideoDetails";
 
-const Full = () => {
-  const downloadButtonClickHandler = (videoUrl) => {
-    api.v1.getDetails(videoUrl).then((response) => console.log(response.data));
-  };
+class Full extends React.Component {
+  constructor(props) {
+    super(props);
+    this.searchButtonClickHandler = this.searchButtonClickHandler.bind(this);
+    this.state = {
+      videoDetails: null,
+    };
+  }
 
-  return (
-    <div className="content">
-      <h1>Full Download</h1>
-      <UrlInputBox onDownloadButtonClick={downloadButtonClickHandler} />
-    </div>
-  );
-};
+  searchButtonClickHandler(videoUrl) {
+    api.v1.getDetails(videoUrl).then((response) => {
+      this.setState({
+        videoDetails: response.data,
+      });
+    });
+  }
+  render() {
+    return (
+      <div className="content">
+        <h1>Full Download</h1>
+        <UrlInputBox onSearchButtonClick={this.searchButtonClickHandler} />
+        {this.state.videoDetails !== null ? (
+          <VideoDetails {...this.state.videoDetails} />
+        ) : null}
+      </div>
+    );
+  }
+}
 
 export default Full;
